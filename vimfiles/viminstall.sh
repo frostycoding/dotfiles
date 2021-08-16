4ce5e86638a4e30842afebcfaf5e7ad6c76a73b5
frostycoding <88981709+frostycoding@users.noreply.github.com>
2021-08-16 14:47:28 +0530

    Update viminstall.sh

--- vimfiles/viminstall.sh
+++ vimfiles/viminstall.sh
@@ -2,6 +2,8 @@
 sudo pacman -S vim
 
 # Install vim-plug
+mkdir -p ~/.vim/autoload/
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
