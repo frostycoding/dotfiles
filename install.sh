baa17b0879c064c4d7d231652f297f43cae27bda
frostycoding <88981709+frostycoding@users.noreply.github.com>
2021-08-16 16:48:50 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #! /usr/bin/env bash
 
+# Copying bspwm config
 cp -r bspwm ~/.config/ 
 
+# Copying fish config
 mkdir ~/.config/fish 
 cp -r fish/config.fish ~/.config/fish/config.fish
 
+# Copying sxhkd config
 cp -r sxhkd ~/.config/ 
+
+# Copying termite config
+cp -r termite ~/.config/
+
+# Copying polybar config
 cp -r polybar ~/.config/
+
+# Copying Wallpapers
 cp -r Wallpapers ~/Pictures/
