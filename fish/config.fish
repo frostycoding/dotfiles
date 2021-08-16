a57a91fd458d51cc800446714aa7882b90413cfc
frostycoding <88981709+frostycoding@users.noreply.github.com>
2021-08-16 16:57:02 +0530

    Delete fish directory

--- fish/config.fish
+++
@@ -1,7 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-alias wifi_list="nmcli d wifi list"
-alias wifi_connect="nmcli d wifi connect Srinidhi password Poonaikutty "
-alias v=vim
-
-end
